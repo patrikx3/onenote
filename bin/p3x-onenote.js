22a9fb23b6186be212a3a8963e31c1c1b3d16bf1
patrikx3 <alabard@gmail.com>
2018-04-23 11:57:52 +0200

    p3x-robot sunday release 2018-4-23 11:57:52

--- bin/p3x-onenote.js
+++ bin/p3x-onenote.js
@@ -5,22 +5,20 @@
 //process.env.UV_THREADPOOL_SIZE = cores;
 //console.debug(`P3X sets UV_THREADPOOL_SIZE to ${cores} thread pool`)
 
-/*
 if (!require('fs').existsSync(`${__dirname}/../node_modules`)) {
     require('child_process').execSync(`cd ${__dirname}/..; npm install --only=prod`, {
         stdio: 'inherit'
     });
 }
-*/
 
 const utils = require('corifeus-utils');
 const path = require('path');
 const mz = require('mz');
 
 const start = async() => {
-/*
-    const desktopEntry = `${process.env.HOME}/.local/share/applications/p3x-onenote.desktop`;
-    const exists = await utils.fs.ensureFile(desktopEntry, `[Desktop Entry]
+    try {
+        const desktopEntry = `${process.env.HOME}/.local/share/applications/p3x-onenote-cli.desktop`;
+        const exists = await utils.fs.ensureFile(desktopEntry, `[Desktop Entry]
 Version=1.0
 Type=Application
 Name=P3X Onenote
@@ -30,12 +28,11 @@ Comment=https://www.patrikx3.com
 Categories=Office;
 Terminal=false
 `)
-    if (!exists) {
-        await mz.fs.chmod(desktopEntry , '0755');
-        await utils.childProcess.exec('gtk-update-icon-cache || true');
-    }
-*/
-    try {
+        if (!exists) {
+            await mz.fs.chmod(desktopEntry , '0755');
+            await utils.childProcess.exec('gtk-update-icon-cache || true');
+        }
+
         await utils.childProcess.exec(`${__dirname}/../node_modules/.bin/electron ${__dirname}/../src/electron/app.js`, true);
     } catch (e) {
         console.error(e);
