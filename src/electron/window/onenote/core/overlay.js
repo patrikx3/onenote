fcd81624156ca902c30636e6d56916a8c943d167
patrikx3 <alabard@gmail.com>
2018-12-31 17:52:53 +0100

    robo8x [chore]   12/31/2018, 5:52:53 PM

--- src/electron/window/onenote/core/overlay.js
+++ src/electron/window/onenote/core/overlay.js
@@ -3,10 +3,12 @@ let hideTimeout
 const ipc = require('electron').ipcRenderer;
 let showCount = 0
 
-p3x.onenote.ui.overlay = new function() {
-    const template = (options = {}) => {
+if (process.versions.hasOwnProperty('electron')) {
 
-        return `
+    p3x.onenote.ui.overlay = new function() {
+        const template = (options = {}) => {
+
+            return `
 <div id="p3xr-overlay">
     <div id="p3xr-overlay-info">
         <i class="fas fa-cog fa-spin fa-4x"></i>
@@ -16,41 +18,44 @@ p3x.onenote.ui.overlay = new function() {
 </div>
         
         `
-    }
+        }
 
-    this.show = (options = {}) => {
-        /*
-        ipc.send('p3x-debug', {
-            'overlay show': shown,
-        })
-        */
-        showCount++
-
-        if (showCount > 5) {
-            require('../action/multi-actions')({
-                action: 'home'
+        this.show = (options = {}) => {
+            /*
+            ipc.send('p3x-debug', {
+                'overlay show': shown,
             })
-            this.hide()
+            */
+            showCount++
+
+            if (showCount > 5) {
+                require('../action/multi-actions')({
+                    action: 'home'
+                })
+                this.hide()
+            }
+
+            if (shown) {
+                return
+            }
+            shown = true
+            ///this.hide()
+            //console.warn('p3xr.ui.overlay show')
+            $body.append(template(options))
         }
 
-        if (shown) {
-            return
+        this.hide = () => {
+
+            /*
+            ipc.send('p3x-debug', {
+                'overlay hide': 'done',
+            })
+            */
+            //console.warn('p3xr.ui.overlay hide')
+            $body.find('#p3xr-overlay').remove()
+            shown = false
         }
-        shown = true
-        ///this.hide()
-        //console.warn('p3xr.ui.overlay show')
-        $body.append(template(options))
     }
 
-    this.hide = () => {
-
-        /*
-        ipc.send('p3x-debug', {
-            'overlay hide': 'done',
-        })
-        */
-        //console.warn('p3xr.ui.overlay hide')
-        $body.find('#p3xr-overlay').remove()
-        shown = false
-    }
 }
+
