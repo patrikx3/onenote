5aac94d63236f376fc0e83a633576ba690c2a881
patrikx3 <alabard@gmail.com>
2019-01-24 18:34:55 +0100

    robo8x [chore]   1/24/2019, 6:34:55 PM

--- src/electron.mock.js
+++ src/electron.mock.js
@@ -26,6 +26,7 @@ const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
 
 global.document = dom.window.document;
 global.window = dom.window
+global.navigator = window.navigator;
 
 require('angular/angular');
 require('angular-mocks');
