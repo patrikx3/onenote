adbc5e36469864b7aea567512a9749e247a2d541
patrikx3 <alabard@gmail.com>
2019-07-12 19:56:50 +0200

    robo8x [chore]   7/12/2019, 7:56:50 PM

--- src/electron/window/onenote/event/handler.js
+++ src/electron/window/onenote/event/handler.js
@@ -95,6 +95,12 @@ const handler = (options) => {
         //TODO Remove this once https://github.com/electron/electron/issues/14474 is fixed
         webview.blur();
         webview.focus();
+
+
+        if (process.env.NODE_ENV === 'debug') {
+            webview.openDevTools()
+        }
+
     });
 
     webview.addEventListener('new-window', async function (event) {
