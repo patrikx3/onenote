fd0a0bfa1fb845b471868650ce303e805940bb76
patrikx3 <alabard@gmail.com>
2020-04-23 07:16:42 +0200

    robo8x [chore]   4/23/2020, 7:16:42 AM

--- src/electron/main/create/tray.js
+++ src/electron/main/create/tray.js
@@ -3,18 +3,26 @@ const {Menu, Tray} = require('electron')
 const menus = require('../menus');
 const action = require('../action');
 
-function mainTray() {
-    if (!global.p3x.onenote.disableHide) {
-        if (global.p3x.onenote.tray === undefined) {
-            global.p3x.onenote.tray = new Tray(global.p3x.onenote.iconFile)
-            global.p3x.onenote.tray.setToolTip(`${global.p3x.onenote.title} v${global.p3x.onenote.pkg.version}`)
-            const click = () => {
-                console.info('tray on click is executed - if not shown in console. this click is not executed.')
-                action.toggleVisible()
-            }
-            global.p3x.onenote.tray.on('click', click)
+const destroyTray  = () => {
+    if (global.p3x.onenote.tray !== undefined) {
+        global.p3x.onenote.tray.destroy()
+        global.p3x.onenote.tray = undefined
+    }
+}
 
+function mainTray() {
+    destroyTray();
+
+    if (!global.p3x.onenote.disableHide) {
+
+        global.p3x.onenote.tray = new Tray(global.p3x.onenote.iconFile)
+        global.p3x.onenote.tray.setToolTip(`${global.p3x.onenote.title} v${global.p3x.onenote.pkg.version}`)
+        const click = () => {
+            console.info('tray on click is executed - if not shown in console. this click is not executed.')
+            action.toggleVisible()
         }
+        global.p3x.onenote.tray.on('click', click)
+
         const menu = menus.default()
 
         menu.splice(menu.length  - 3, 0, require('./menu-display')())
@@ -22,11 +30,6 @@ function mainTray() {
         const contextMenu = Menu.buildFromTemplate(menu)
         global.p3x.onenote.tray.setContextMenu(contextMenu)
 
-    } else {
-        if (global.p3x.onenote.tray !== undefined) {
-            global.p3x.onenote.tray.destroy()
-            global.p3x.onenote.tray = undefined
-        }
     }
 }
 
