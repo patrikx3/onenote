690f570a7fe49d23f3a50323d11764aecde82c59
patrikx3 <alabard@gmail.com>
2021-02-18 07:25:09 +0100

    r0b08x [chore]   2/18/2021, 7:25:09 AM

--- src/electron/main/create/tray.js
+++ src/electron/main/create/tray.js
@@ -1,4 +1,4 @@
-const {Menu, Tray} = require('electron')
+const {app, Menu, Tray} = require('electron')
 
 const menus = require('../menus');
 const action = require('../action');
@@ -11,24 +11,34 @@ const destroyTray  = () => {
 }
 
 function mainTray() {
-    destroyTray();
 
-    if (!global.p3x.onenote.disableHide) {
+    app.whenReady().then(() => {
+        //destroyTray();
 
-        global.p3x.onenote.tray = new Tray(global.p3x.onenote.iconFile)
-        global.p3x.onenote.tray.setToolTip(`${global.p3x.onenote.title} v${global.p3x.onenote.pkg.version}`)
-        const click = () => {
-            console.info('tray on click is executed - if not shown in console. this click is not executed.')
-            action.toggleVisible()
+        if (!global.p3x.onenote.disableHide) {
+
+            if (global.p3x.onenote.tray === undefined ) {
+                global.p3x.onenote.tray = new Tray(global.p3x.onenote.iconFile)
+            }
+
+            global.p3x.onenote.tray.setToolTip(`${global.p3x.onenote.title} v${global.p3x.onenote.pkg.version}`)
+            const click = () => {
+                console.info('tray on click is executed - if not shown in console. this click is not executed.')
+                action.toggleVisible()
+            }
+            global.p3x.onenote.tray.on('click', click)
+
+            const menu = menus.default()
+
+            const contextMenu = Menu.buildFromTemplate(menu)
+            global.p3x.onenote.tray.setContextMenu(contextMenu)
+
+        } else if (global.p3x.onenote.tray !== undefined) {
+            app.relaunch()
+            app.exit()
         }
-        global.p3x.onenote.tray.on('click', click)
+    })
 
-        const menu = menus.default()
-
-        const contextMenu = Menu.buildFromTemplate(menu)
-        global.p3x.onenote.tray.setContextMenu(contextMenu)
-
-    }
 }
 
 module.exports = mainTray;
