366a9a2e07c585f13947b84875f13cd08212fa53
WinterSoldier13 <ayushsingh1315@gmail.com>
2021-04-12 22:53:39 +0530

    fixed code

--- src/electron/window/onenote/darkMode.js
+++ src/electron/window/onenote/darkMode.js
@@ -1,36 +1,35 @@
 
 // This part is to invert the colors
 function toggleDarkLight() {
-    javascript: (
-        function () {
-            // the css we are going to inject
-            console.log("Loaded-DarkMode");
 
-            var css = 'html {-webkit-filter: invert(100%);' +
-                '-moz-filter: invert(100%);' +
-                '-o-filter: invert(100%);' +
-                '-ms-filter: invert(100%); }',
+    // the css we are going to inject
+    console.log("Loaded-DarkMode");
 
-                head = document.getElementsByTagName('head')[0],
-                style = document.createElement('style');
+    var css = 'html {-webkit-filter: invert(100%);' +
+        '-moz-filter: invert(100%);' +
+        '-o-filter: invert(100%);' +
+        '-ms-filter: invert(100%); }',
 
-            // a hack, so you can "invert back" clicking the bookmarklet again
-            if (!window.counter) { window.counter = 1; } else {
-                window.counter++;
-                if (window.counter % 2 == 0) { var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' }
-            };
+        head = document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
 
-            style.type = 'text/css';
-            if (style.styleSheet) {
-                style.styleSheet.cssText = css;
-            } else {
-                style.appendChild(document.createTextNode(css));
-            }
+    // a hack, so you can "invert back" clicking the bookmarklet again
+    if (!window.counter) { window.counter = 1; } else {
+        window.counter++;
+        if (window.counter % 2 == 0) { var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' }
+    };
 
-            //injecting the css to the head
-            head.appendChild(style);
-        }());
-}
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    //injecting the css to the head
+    head.appendChild(style);
+};
+
 
 
 // __________________________________________________________________________________________________________________________________________________________________________________________
