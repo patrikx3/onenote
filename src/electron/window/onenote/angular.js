41f46640f5a56fcc0ee5e9b3e6d187e646e88223
patrikx3 <alabard@gmail.com>
2021-06-04 07:59:06 +0200

    r0b08x [chore]   6/4/2021, 7:59:06 AM

--- src/electron/window/onenote/angular.js
+++ src/electron/window/onenote/angular.js
@@ -12,6 +12,23 @@ global.p3x.onenote.ng = angular.module('p3x-onenote', [
 require('./angular/prompt');
 require('./angular/toast');
 
+const remote = require('@electron/remote')
+const BrowserWindow = remote.BrowserWindow;
+const win = BrowserWindow.getFocusedWindow();
+
+let zoom = p3x.onenote.conf.get('zoom')
+if (zoom === undefined) {
+    zoom = 1.0
+}
+win.webContents.setZoomFactor(zoom);
+
+win.webContents
+    .setVisualZoomLevelLimits(1, 5)
+    .then(console.log("Zoom Levels Have been Set between 100% and 500%"))
+    .catch((err) => console.error(err));
+
+
+
 global.p3x.onenote.ng.config(($mdAriaProvider, $mdThemingProvider) => {
 
     $mdAriaProvider.disableWarnings();
@@ -45,6 +62,22 @@ global.p3x.onenote.ng.run((p3xOnenotePrompt, p3xOnenoteToast, $rootScope, $anima
             copyLocation: require('./action/multi-action/get-location'),
             donate: () => {
                 shell.openExternal('https://paypal.me/patrikx3')
+            },
+            zoom: (zoom) => {
+                const currentZoom = win.webContents.getZoomFactor();
+                let value
+               if (zoom >= 0) {
+                    value = currentZoom + 0.1;
+                } else {
+                    value = currentZoom - 0.1;
+                }
+               if (value >= 0.75 && value <= 5.0) {
+                   win.webContents.zoomFactor = value
+                   p3x.onenote.conf.set('zoom', win.webContents.zoomFactor)
+               }
+            },
+            get zoomFactor() {
+                return (win.webContents.zoomFactor * 100).toFixed(0)
             }
         }
     }
